94216ce58045a46124ef322ef75880c1b7820558
10hesen044 <tu201906250@code.edu.az>
2021-10-28 13:43:26 +0400

    project added

---
+++ 28.10.2021tap2/28.10.2021tap2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _28._10._2021tap2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string sentence = "Salam Aztu uni";
+            Console.WriteLine(GetNum(sentence));
+        }
+        static int GetNum(string str)
+        {
+            int length, count = 0;
+             
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            length = str.Length - 1;
+            if (str[length] != '.' || str[length] != '?' || str[length] != '!')
+            {
+                return count + 1;
+            }
+            return count;
+        }
+    }
+}
